1061ba65cee9f91b7b5a77ee00f8d00f9f404035
1FarZ1 <91225280+1FarZ1@users.noreply.github.com>
2023-06-04 17:07:42 +0100

    refectoring

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './componants/App.jsx'
 import './css/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <h1>
+      test
+    </h1>
   </React.StrictMode>,
 )
---
+++ client/src/pages/sign_in/componants/submit_button.jsx
@@ -0,0 +1,11 @@
+
+
+const submitButton = () => (
+    <>
+        <button type="submit" className="btn btn-primary btn-block">Sign In</button>
+    </>
+);
+
+
+
+export default submitButton;
