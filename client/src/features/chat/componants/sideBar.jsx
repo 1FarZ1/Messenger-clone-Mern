377cf4c5f3123ae28431da0a697c874acf7f7705
1FarZ1 <91225280+1FarZ1@users.noreply.github.com>
2023-06-06 05:26:25 +0100

    refectoring the code

--- client/src/features/chat/componants/sideBar.jsx
+++ client/src/features/chat/componants/sideBar.jsx
@@ -1,11 +1,24 @@
 
 
+import Contact from "./contact";
+import Header from "./header";
+import Footer from "./Footer.jsx";
+
+
 const SideBar  = () => {
     return (
-        <div>
-            
-
-        </div>
+        <>
+         <Header />
+            <input
+                type="text"
+                placeholder="Search here" />
+            <Contact username={"fares"} profilePic={"https://avatars.githubusercontent.com/u/91225280?v=4"} msg={"hey whats up man"} time={"1h"} />
+            <Contact username={"fares"} profilePic={"https://avatars.githubusercontent.com/u/91225280?v=4"} msg={"hey whats up man"} time={"1h"} />
+            <Contact username={"fares"} profilePic={"https://avatars.githubusercontent.com/u/91225280?v=4"} msg={"hey whats up man"} time={"1h"} />
+            <Contact username={"fares"} profilePic={"https://avatars.githubusercontent.com/u/91225280?v=4"} msg={"hey whats up man"} time={"1h"} />
+            <Contact username={"fares"} profilePic={"https://avatars.githubusercontent.com/u/91225280?v=4"} msg={"hey whats up man"} time={"1h"} />
+            <Footer />
+        </>
     );
 
 }
--- client/src/features/chat/main.jsx
+++ client/src/features/chat/main.jsx
@@ -1,26 +1,13 @@
 /* eslint-disable react/prop-types */
 
-import Contact from "./componants/contact";
-import Header from "./componants/header";
-import Footer from "./componants/Footer.jsx";
+import SideBar from "./componants/sideBar";
 
-// import { useLocation } from "react-router-dom";
 
 const ChatPage = () => {
-    // const location = useLocation();
 
     return (
         <div>
-            <Header />
-            <input
-                type="text"
-                placeholder="Search here" />
-            <Contact username={"fares"} profilePic={"https://avatars.githubusercontent.com/u/91225280?v=4"} msg={"hey whats up man"} time={"1h"} />
-            <Contact username={"fares"} profilePic={"https://avatars.githubusercontent.com/u/91225280?v=4"} msg={"hey whats up man"} time={"1h"} />
-            <Contact username={"fares"} profilePic={"https://avatars.githubusercontent.com/u/91225280?v=4"} msg={"hey whats up man"} time={"1h"} />
-            <Contact username={"fares"} profilePic={"https://avatars.githubusercontent.com/u/91225280?v=4"} msg={"hey whats up man"} time={"1h"} />
-            <Contact username={"fares"} profilePic={"https://avatars.githubusercontent.com/u/91225280?v=4"} msg={"hey whats up man"} time={"1h"} />
-            <Footer />
+        <SideBar/>
 
 
         </div>
