4938a290727d7c2655a524c164cbaf94b6cbc504
1FarZ1 <91225280+1FarZ1@users.noreply.github.com>
2023-06-07 03:27:54 +0100

    request to get the informations for the user

--- client/src/context/authProvider.jsx
+++ client/src/context/authProvider.jsx
@@ -1,20 +1,33 @@
 /* eslint-disable react/prop-types */
 import { useState, useEffect } from 'react';
 import AuthContext from './authContext'
+import axios from 'axios';
 
 
-const getUser  =()=>{
-    
+const getUser  = async ()=>{
+        try {
+            const res=  await axios.get("http://127.0.0.1:5500/api/v1/auth/me");
+            if(res.status ==200){
+                return res.data; 
+            }
+            return null;
+            
+        } catch (error) {
+            console.log(error);    
+            return null;
+        }
+
+
 }
 
 
 export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     useEffect(() => {
- const currentUser = getUser()
- setUser(currentUser)
+        const currentUser = getUser()
+     setUser(currentUser)
  }, []);
     return (
-      <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
+      <AuthContext.Provider value={{ user}}>{children}</AuthContext.Provider>
     );
   };
